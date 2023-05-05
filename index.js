5367c76ab73c9f54d13b1f67b792d9a5a719a30c
gitveryown <1williambwrgiht@gmail.com>
2023-05-05 14:36:20 -0400

    js update

---
+++ index.js
@@ -0,0 +1,17 @@
+// Drop menu opens onclick
+// list items display onclick 
+
+
+const dropIcon = document.getElementById('dropIcon')
+const listItems = document.getElementsByClassName('displayList')
+
+
+function slideOpen (){
+    // openIcon.style.transform = 'translate(240px, 0)'
+    console.log('going to open up a menu bar')
+   
+}
+
+const openIcon = document.getElementById("dropMenu")
+openIcon.addEventListener('click', slideOpen)
+
